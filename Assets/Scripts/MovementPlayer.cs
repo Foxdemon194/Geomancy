fc7b81e827bb5f08e830e44398e64f027f60420f
DV <david_ventura_m@hotmail.com>
2023-02-28 05:58:42 -0500

    implemented movement and its UI into lv1 and lv2

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -13,22 +13,77 @@ public class MovementPlayer : MonoBehaviour
 
     Vector2 movement;
 
+    public GameObject pauseMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-    }
+    //void Update()
+    //{
+    //movement.x = Input.GetAxisRaw("Horizontal");
+    //movement.y = Input.GetAxisRaw("Vertical");
+    //}
 
-    private void FixedUpdate()
+    private void Update()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void MoveLeftDown()
+    {
+        movement.x = -1;
+    }
+    public void MoveLeftUp()
+    {
+        movement.x = 0;
+    }
+
+    public void MoveRightDown()
+    {
+        movement.x = 1;
+    }
+    public void MoveRightUp()
+    {
+        movement.x = 0;
+    }
+
+    public void MoveDownDown()
+    {
+        movement.y = -1;
+    }
+    public void MoveDownUp()
+    {
+        movement.y = 0;
+    }
+
+    public void MoveUpDown()
+    {
+        movement.y = 1;
+    }
+    public void MoveUpUp()
+    {
+        movement.y = 0;
+    }
+
+    public void MenuOpen()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void MenuClose()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
